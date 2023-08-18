826712b248349d88cc46165c2a9152de8da56523
Safo Emmanuel <nanasafo122@gmail.com>
2023-08-18 08:40:09 +0000

    first commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+
+const DashboardPage = () => {
+    return ( 
+        <p>Dashboard [UnProtected]</p>
+     );
+}
+ 
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+    return ( 
+        <div>
+            UnProtected
+        </div>
+     );
+}
+ 
+export default LandingPage;
